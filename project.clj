2b72d9028cadfb73863a18e67f41d3d668e79ac2
Oscar F. Toro <oscarftoro@protonmail.com>
2019-03-21 07:34:52 +0100

    add bdd primitives that operates on 0  and 1 and add some notes

--- project.clj
+++ project.clj
@@ -4,5 +4,27 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/test.check "0.9.0"]]
-  :repl-options {:init-ns bandido-clojure.core})
+                 [org.clojure/test.check "0.9.0"]
+                 [cider/cider-nrepl "0.21.1"]]
+  
+  :repl-options {:init-ns bandido-clojure.core
+                 :nrepl-middleware
+                 [cider.nrepl/wrap-apropos
+                  cider.nrepl/wrap-classpath
+                  cider.nrepl/wrap-complete
+                  cider.nrepl/wrap-debug
+                  cider.nrepl/wrap-format
+                  cider.nrepl/wrap-info
+                  cider.nrepl/wrap-inspect
+                  cider.nrepl/wrap-macroexpand
+                  cider.nrepl/wrap-ns
+                  cider.nrepl/wrap-spec
+                  cider.nrepl/wrap-profile
+                  cider.nrepl/wrap-refresh
+                  cider.nrepl/wrap-resource
+                  cider.nrepl/wrap-stacktrace
+                  cider.nrepl/wrap-test
+                  cider.nrepl/wrap-trace
+                  cider.nrepl/wrap-out
+                  cider.nrepl/wrap-undef
+                  cider.nrepl/wrap-version]})
--- src/bandido_clojure/core.clj
+++ src/bandido_clojure/core.clj
@@ -1,5 +1,5 @@
 (ns bandido-clojure.core
-  require [clojure.spec.alpha : as s])
+  (:require [clojure.spec.alpha :as s]))
 
 (defn foo
   "I don't do a whole lot."
@@ -11,7 +11,35 @@
 (s/def :dk.nqn.bandido-clojure/b #{0 1})
 (s/def :dk.nqn.bandido-clojure/uid (s/and int? #(> % 0)))
 (s/def :dk.nqn.bandido-clojure/var (s/and int? #(> % 0)))
-(s/def :dk.nqn.bandido-clojure/ite
-  (s/keys :req-un []))
+;(s/def :dk.nqn.bandido-clojure/ite
+ ; (s/keys :req-un []))
 ;; base fun(S/def :dk.nqn.bandido-clojure/uid (s/and int? #(> % 0)))ctions
-(defn const[b] 1)
+(defn const [b] b)
+
+
+(defn- bdd-not [b]
+  (case b
+    1 0
+    0 1))
+
+(defn- bdd-or [b c]
+  (case b
+    1 1
+    0 (case c
+        1 1
+        0 0)))
+
+(defn- bdd-and [b c]
+  (case b
+    0 0
+    1 (bdd-or 0 c)))    
+
+
+(defn ite [f g h]
+  "Three argument operation that stands for If-then-else.
+   When f is true returns g otherwise h"
+  (bdd-or (bdd-and f g) (bdd-and (bdd-not f) h)))
+
+(defn and* [f g]
+  (ite f g 0))
+
