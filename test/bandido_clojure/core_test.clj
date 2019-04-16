f9fe3897ed0d743ce6ab87fdf9672790a7883f57
Oscar Toro <oscar.toro@ufst.dk>
2019-04-16 06:51:25 +0200

    add one more test

--- test/bandido_clojure/core_test.clj
+++ test/bandido_clojure/core_test.clj
@@ -22,3 +22,21 @@
         t (:t bdd*)
         t-size (count t)]
     (is (= 3 t-size))))
+
+(deftest mk-should-return-the-same-bdd-when-l-eq-h
+  (testing "mk do nothing when l = h but returns the same bdd"
+    (let [bdd (init-table 4)
+          bdd* (mk1 [4 1 1] bdd)
+          t (:t bdd*)
+          t-size (count t)]
+      (is (= 2 t-size)))))
+
+(deftest mk-should-be-efficient
+  (testing "mk returns the value saved in table h when it corresponds"
+    (let [bdd (init-table 4)
+          bdd1 (mk1 [4 1 0] bdd)
+          bdd2 (mk1 [4 0 1] bdd1)
+          bdd3 (mk1 [4 1 0] bdd2)
+          t (:t bdd3)
+          t-size (count t)]
+      (is (= 4 t-size)))))
