e59672eba47acc50748d99db3ad0e064789a98a5
Oscar Toro <oscar.toro@ufst.dk>
2019-04-28 14:08:15 +0200

    align tests

--- test/bandido_clojure/core_test.clj
+++ test/bandido_clojure/core_test.clj
@@ -1,20 +1,20 @@
 (ns bandido-clojure.core-test
   (:require
-   [clojure.test :refer :all]
-   [clojure.test.check :as tc]
-   [clojure.test.check.generators :as gen]
-   [clojure.test.check.properties :as prop]
-   [bandido-clojure.core :refer [init-table mk1 mk1a v low high apply* map->Bdd var* ]]
-   [bandido-clojure.specs :as specs]
-   [clojure.spec.test.alpha :as stest]))
+    [clojure.test :refer :all]
+    [clojure.test.check :as tc]
+    [clojure.test.check.generators :as gen]
+    [clojure.test.check.properties :as prop]
+    [bandido-clojure.core :refer [init-table mk1 mk1a v low high apply* map->Bdd var*]]
+    [bandido-clojure.specs :as specs]
+    [clojure.spec.test.alpha :as stest]))
 
 
 (defn setup [f]
-  (def bdd-test (let [bdd (init-table 3)
+  (def bdd-test (let [bdd  (init-table 3)
                       bdd1 (mk1 [3 0 1] bdd)
                       bdd2 (mk1 [2 1 0] bdd1)
                       bdd3 (mk1 [2 0 1] bdd2)
-                      bdd4 (mk1 [1 0 2] bdd3) 
+                      bdd4 (mk1 [1 0 2] bdd3)
                       bdd5 (mk1 [1 3 4] bdd4)] bdd5))
   (f))
 
@@ -56,103 +56,122 @@
 
 (deftest init-table-test
   (testing "init-table should return a Bdd with a table t with two elements")
-  (let [bdd (init-table 5)
-        t (:t bdd)
+  (let [bdd    (init-table 5)
+        t      (:t bdd)
         t-size (count t)]
-    (is (= 2  t-size))))
+    (is (= 2 t-size))))
 
 (deftest mk-should-make-test
   (testing "that mk add a new node to the table")
-  (let [bdd (init-table 4)
-        bdd* (mk1 [4 1 0] bdd)
-        t (:t bdd*)
+  (let [bdd    (init-table 4)
+        bdd*   (mk1 [4 1 0] bdd)
+        t      (:t bdd*)
         t-size (count t)]
     (is (= 3 t-size))))
 
 (deftest mk-should-return-the-same-bdd-when-l-eq-h-test
   (testing "mk do nothing when l = h but returns the same bdd"
-    (let [bdd (init-table 4)
-          bdd* (mk1 [4 1 1] bdd)
-          t (:t bdd*)
+    (let [bdd    (init-table 4)
+          bdd*   (mk1 [4 1 1] bdd)
+          t      (:t bdd*)
           t-size (count t)]
       (is (= 2 t-size)))))
 
 (deftest mk-should-be-efficient-test
   (testing "mk returns the value saved in table h when it corresponds"
-    (let [bdd (init-table 4)
-          bdd1 (mk1 [4 1 0] bdd)
-          bdd2 (mk1 [4 0 1] bdd1)
-          bdd3 (mk1 [4 1 0] bdd2)
-          t (:t bdd3)
+    (let [bdd    (init-table 4)
+          bdd1   (mk1 [4 1 0] bdd)
+          bdd2   (mk1 [4 0 1] bdd1)
+          bdd3   (mk1 [4 1 0] bdd2)
+          t      (:t bdd3)
           t-size (count t)]
       (is (= 4 t-size)))))
 
 (deftest mk1a-should-make-test
   (testing "that mk1a add a new node to the table")
-  (let [bdd (init-table 4)
-        bdd* (mk1a [4 1 0] bdd)
-        t (:t bdd*)
+  (let [bdd    (init-table 4)
+        bdd*   (mk1a [4 1 0] bdd)
+        t      (:t bdd*)
         t-size (count t)]
     (is (= 3 t-size))))
 
 (deftest mk1a-should-return-the-same-bdd-when-l-eq-h-test
   (testing "mk1a do nothing when l = h but returns the same bdd"
-    (let [bdd (init-table 4)
-          bdd* (mk1a [4 1 1] bdd)
-          t (:t bdd*)
+    (let [bdd    (init-table 4)
+          bdd*   (mk1a [4 1 1] bdd)
+          t      (:t bdd*)
           t-size (count t)]
       (is (= 2 t-size)))))
 
 (deftest mk1a-should-be-efficient-test
   (testing "mk1a returns the value saved in table h when it corresponds"
-    (let [bdd (init-table 4)
-          bdd1 (mk1a [4 1 0] bdd)
-          bdd2 (mk1a [4 0 1] bdd1)
-          bdd3 (mk1a [4 1 0] bdd2)
-          t (:t bdd3)
+    (let [bdd    (init-table 4)
+          bdd1   (mk1a [4 1 0] bdd)
+          bdd2   (mk1a [4 0 1] bdd1)
+          bdd3   (mk1a [4 1 0] bdd2)
+          t      (:t bdd3)
           t-size (count t)]
       (is (= 4 t-size)))))
 
 
 (deftest apply-should-behave-as-expected
   (testing "apply should apply a logic formula to a bdd")
-  (let [bdd (init-table 3)
-        bdd1 (mk1 [3 0 1] bdd)
-        bdd2 (mk1 [2 1 0] bdd1)
-        bdd3 (mk1 [2 0 1] bdd2)
-        bdd4 (mk1 [1 0 2] bdd3)
-        bdd5 (mk1 [1 3 4] bdd4)
-        actual (apply* :and 5 6 bdd5)
-        
-        expected (map->Bdd {:t {0 [4 0 0]
-                                1 [4 1 1]
-                                2 [3 0 1]
-                                3 [2 1 0]
-                                4 [2 0 1]
-                                5 [1 0 2]
-                                6 [1 3 4]
-                                7 [2 0 2]
-                                8 [1 0 7]}
-                            :uid 8
-                            :luid 8})]
-    (is (= expected actual ))))
+  (let [bdd      (init-table 3)
+        bdd1     (mk1 [3 0 1] bdd)
+        bdd2     (mk1 [2 1 0] bdd1)
+        bdd3     (mk1 [2 0 1] bdd2)
+        bdd4     (mk1 [1 0 2] bdd3)
+        bdd5     (mk1 [1 3 4] bdd4)
+        actual   (apply* :and 5 6 bdd5)
 
-(deftest build-a-logical-formula
+        expected (map->Bdd {:t    {0 [4 0 0]
+                                   1 [4 1 1]
+                                   2 [3 0 1]
+                                   3 [2 1 0]
+                                   4 [2 0 1]
+                                   5 [1 0 2]
+                                   6 [1 3 4]
+                                   7 [2 0 2]
+                                   8 [1 0 7]}
+                            :uid  8
+                            :luid 8})]
+    (is (= expected actual))))
+
+(deftest build-variables
   (testing "constructing variables x_1, x_2 and ~x_3 should be a blast")
-  (let [bdd (init-table 3)
-        bdd1 (var* 1 bdd)
-        bdd2 (var* 2 bdd1)
-        actual (var* 3 bdd2)
+  (let [bdd      (init-table 3)
+        bdd1     (var* 1 bdd)
+        bdd2     (var* 2 bdd1)
+        actual   (var* 3 bdd2)
 
         expected (map->Bdd {
-                            :t {0 [4 0 0]
-                                1 [4 1 1]
-                                2 [1 0 1]
-                                3 [2 0 1]
-                                4 [3 0 1]}
-                            :uid 4
+                            :t    {0 [4 0 0]
+                                   1 [4 1 1]
+                                   2 [1 0 1]
+                                   3 [2 0 1]
+                                   4 [3 0 1]}
+                            :uid  4
                             :luid 4})]
-  (is (= expected actual))))
+    (is (= expected actual))))
 
-    
-(use-fixtures :each setup )
+(deftest build-a-logical-formula
+  (testing "constructing variables (x_1 and x_2 and ~x_3) or (~x_1 and x_3) 
+   should be a total blast")
+  (let [bdd      (init-table 3)
+        bdd1     (var* 1 bdd)
+        bdd2     (var* 2 bdd1)
+        bdd3     (var* 3 bdd2)
+        actual   (apply* :and (v 1 bdd3) (v 2 bdd3) bdd3)
+
+        expected (map->Bdd {
+                            :t    {0 [4 0 0]
+                                   1 [4 1 1]
+                                   2 [1 0 1]
+                                   3 [2 0 1]
+                                   4 [3 0 1]}
+                            :uid  4
+                            :luid 4})]
+    (is (= expected actual))))
+
+
+(use-fixtures :each setup)
